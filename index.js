ea3fbee39059a215597e3fe66456d3938211ecc8
byan1197 <Bondyan97@gmail.com>
2021-12-15 16:25:03 -0500

    heroku port semantics

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const morgan = require("morgan");
 const app = express();
 
 // Configuration
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.port || 3000;
 const HOST = process.env.host || "localhost";
 app.use(morgan("dev"));
 
