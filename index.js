d0b47d84ec94dced3c6fac2f131faa52249798da
byan1197 <Bondyan97@gmail.com>
2021-12-15 19:33:57 -0500

    fixed cors

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ const { getSoompiFeed } = require("./services/rss-feed");
 const { getCurrentWeather } = require("./services/weather");
 const express = require("express");
 const morgan = require("morgan");
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-
-// Create Express Server
+const cors = require("cors");
 const app = express();
-
+const allowedOrigins = ["http://localhost:3000"];
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowedOrigins.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 // Configuration
 const PORT = process.env.PORT || process.env.port || 3000;
 const HOST = process.env.host || "localhost";
 app.use(morgan("dev"));
 
+app.use(cors(corsOptionsDelegate));
+
 app.use("", (req, res, next) => {
-  const allowedOrigins = ["http://localhost:3000"];
-  const origin = req.headers.origin;
-  if (
-    allowedOrigins.includes(origin) &&
-    req.headers.api_key &&
-    req.headers.api_key === process.env.api_key
-  ) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
+  if (req.headers.api_key && req.headers.api_key === process.env.api_key) {
+    next();
   } else {
     res.sendStatus(403);
   }
